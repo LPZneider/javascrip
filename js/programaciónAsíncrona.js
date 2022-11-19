e7d3410e076ddb1caed41029f04a232aed1529f9
Neider Lopez <lpzneider@gmail.com>
2022-11-18 23:33:21 -0500

    inicando en la asíncronía

---
+++ js/programaciónAsíncrona.js
@@ -0,0 +1,28 @@
+
+// Temporizadores (setTimeout & setInterval) 
+/* 
+ console.log("inicio")
+setTimeout(()=>{
+    console.log("esto se ejecuta una sola vez")
+}, 3000)
+
+setInterval(()=>{
+    console.log("esto se ejecuta indefinidamente cada cierto intervalo de tiempo")
+    
+}, 5000) 
+
+// contrapartes
+
+let temporizador = setTimeout(()=>{
+    console.log("esto se ejecuta una sola vez")
+}, 3000)
+
+clearTimeout(temporizador) 
+
+ let temporizador = setInterval(()=>{
+    console.log("esto se ejecuta indefinidamente cada cierto intervalo de tiempo")
+}, 5000)
+
+clearInterval(temporizador) */
+
+ 
